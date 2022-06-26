aed8d96876c5443cf398e902b5a35350d0563880
minssu86 <manager.kim86@gmail.com>
2022-06-26 19:45:38 +0900

    6/26 The project repository has been created.

---
+++ src/main/java/com/sparta/meeting_platform/MeetingPlatformApplication.java
@@ -0,0 +1,13 @@
+package com.sparta.meeting_platform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MeetingPlatformApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MeetingPlatformApplication.class, args);
+    }
+
+}
