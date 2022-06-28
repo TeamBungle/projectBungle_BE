1f30a99c6b1367e5dd9c4cad9784d9b5a14a611e
kootner <refomto@naver.com>
2022-06-28 10:14:16 +0900

    네이버 소셜 컨트롤러 추가

--- src/main/java/com/sparta/meeting_platform/controller/SocialController.java
+++ src/main/java/com/sparta/meeting_platform/controller/SocialController.java
@@ -3,13 +3,12 @@ package com.sparta.meeting_platform.controller;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.sparta.meeting_platform.dto.Google.GoogleResponseDto;
 import com.sparta.meeting_platform.service.SocialGoogleService;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpServletResponse;
 
 @RestController
+@RequestMapping("/user/signin")
 public class SocialController {
 
     private final SocialGoogleService socailService;
@@ -19,10 +18,16 @@ public class SocialController {
     }
 
     //https://accounts.google.com/o/oauth2/v2/auth?client_id=1063605264794-i2qeg4lqsmi3u60pu51cdqcm9eemrb23.apps.googleusercontent.com&redirect_uri=http://localhost:8080/user/google/signin&response_type=code&scope=email%20profile%20openid&access_type=offline
-    @GetMapping("/user/signin/google")
+    @GetMapping("/google")
     public GoogleResponseDto googleLogin(@RequestParam(value = "code") String authCode, HttpServletResponse httpServletResponse) throws JsonProcessingException {
         GoogleResponseDto googleUserResponseDto = socailService.googleLogin(authCode, httpServletResponse);
         return googleUserResponseDto;
+    }
+
+    @ResponseBody
+    @GetMapping("/naver")
+    public String naverRestCallback(@RequestParam String code, @RequestParam String state)  {
+        return naverOAuthService.getNaverAccessToken(code, state);
     }
 
 }
