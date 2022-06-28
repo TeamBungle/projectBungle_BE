dbf327eff4c4f4414864dcde80e9b3756a8df65d
kootner <refomto@naver.com>
2022-06-28 14:50:05 +0900

    약간에 리팩토링 및 프로필 사진 가져오기 추가

--- src/main/java/com/sparta/meeting_platform/controller/SocialController.java
+++ src/main/java/com/sparta/meeting_platform/controller/SocialController.java
@@ -3,6 +3,7 @@ package com.sparta.meeting_platform.controller;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.sparta.meeting_platform.dto.Google.GoogleResponseDto;
 import com.sparta.meeting_platform.service.SocialGoogleService;
+import com.sparta.meeting_platform.service.SocialNaverService;
 import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpServletResponse;
@@ -12,9 +13,11 @@ import javax.servlet.http.HttpServletResponse;
 public class SocialController {
 
     private final SocialGoogleService socailService;
+    private final SocialNaverService socialNaverService;
 
-    public SocialController(SocialGoogleService socailService){
+    public SocialController(SocialGoogleService socailService, SocialNaverService socialNaverService){
         this.socailService = socailService;
+        this.socialNaverService = socialNaverService;
     }
 
     //https://accounts.google.com/o/oauth2/v2/auth?client_id=1063605264794-i2qeg4lqsmi3u60pu51cdqcm9eemrb23.apps.googleusercontent.com&redirect_uri=http://localhost:8080/user/google/signin&response_type=code&scope=email%20profile%20openid&access_type=offline
@@ -26,8 +29,8 @@ public class SocialController {
 
     @ResponseBody
     @GetMapping("/naver")
-    public String naverRestCallback(@RequestParam String code, @RequestParam String state)  {
-        return naverOAuthService.getNaverAccessToken(code, state);
+    public String naverLogin(@RequestParam String code, @RequestParam String state)  {
+        return socialNaverService.getNaverAccessToken(code, state);
     }
 
 }
--- src/main/java/com/sparta/meeting_platform/service/SocialNaverService.java
+++ src/main/java/com/sparta/meeting_platform/service/SocialNaverService.java
@@ -1,55 +1,24 @@
 package com.sparta.meeting_platform.service;
 
+import com.google.gson.JsonElement;
+import com.google.gson.JsonParser;
+import org.springframework.stereotype.Service;
+
 import java.io.*;
 import java.net.HttpURLConnection;
 import java.net.URL;
-import com.google.gson.JsonElement;
-import com.google.gson.JsonParser;
 
+@Service
 public class SocialNaverService {
 
+    // 클라이언트로 부터 받은 code, state 를 네이버로 전달하여 access_Token 받는 함수
     public String getNaverAccessToken(String code, String state) {
         String access_Token = "";
-        String refresh_Token = "";
+        String refresh_Token;
         String reqURL = "https://nid.naver.com/oauth2.0/token";
 
         try {
-            URL url = new URL(reqURL);
-            HttpURLConnection conn = (HttpURLConnection) url.openConnection();
-
-            //POST 요청을 위해 기본값이 false인 setDoOutput을 true로
-            conn.setRequestMethod("POST");
-            conn.setDoOutput(true);
-
-            //POST 요청에 필요로 요구하는 파라미터 스트림을 통해 전송
-            BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(conn.getOutputStream()));
-            StringBuilder sb = new StringBuilder();
-            sb.append("grant_type=authorization_code");
-            sb.append("&client_id=fNINb0JLOoWHKPO8p2HO"); // TODO REST_API_KEY 입력
-            sb.append("&client_secret=5PzjffB3yr");
-            sb.append("&redirect_uri=http://localhost:3000/oauth/callback/naver"); // TODO 인가코드 받은 redirect_uri 입력
-            sb.append("&code=" + code);
-            sb.append("&state=" + state);
-            bw.write(sb.toString());
-            bw.flush();
-
-            //결과 코드가 200이라면 성공
-            int responseCode = conn.getResponseCode();
-            System.out.println("responseCode : " + responseCode);
-
-            //요청을 통해 얻은 JSON타입의 Response 메세지 읽어오기
-            BufferedReader br = new BufferedReader(new InputStreamReader(conn.getInputStream()));
-            String line = "";
-            String result = "";
-
-            while ((line = br.readLine()) != null) {
-                result += line;
-            }
-            System.out.println("response body : " + result);
-
-            //Gson 라이브러리에 포함된 클래스로 JSON파싱 객체 생성
-            JsonParser parser = new JsonParser();
-            JsonElement element = parser.parse(result);
+            JsonElement element = jsonElement(reqURL, null, code, state);
 
             access_Token = element.getAsJsonObject().get("access_token").getAsString();
             refresh_Token = element.getAsJsonObject().get("refresh_token").getAsString();
@@ -57,54 +26,75 @@ public class SocialNaverService {
             System.out.println("access_token : " + access_Token);
             System.out.println("refresh_token : " + refresh_Token);
 
-            br.close();
-            bw.close();
+
         } catch (IOException e) {
             e.printStackTrace();
         }
-
         System.out.println(access_Token);
         createNaverUser(access_Token);
         return access_Token;
     }
 
-
-    public void createNaverUser(String token)  {
+    // 받은 토큰을 사용하여 유저 정보 가져오는 함수
+    public void createNaverUser(String token) {
 
         String reqURL = "https://openapi.naver.com/v1/nid/me";
 
-        //access_token을 이용하여 사용자 정보 조회
         try {
-            URL url = new URL(reqURL);
-            HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+            JsonElement element = jsonElement(reqURL, token, null, null);
 
-            conn.setRequestMethod("POST");
-            conn.setDoOutput(true);
-            conn.setRequestProperty("Authorization", "Bearer " + token); //전송할 header 작성, access_token전송
-
-            //결과 코드가 200이라면 성공
-            int responseCode = conn.getResponseCode();
-            System.out.println("responseCode : " + responseCode);
-
-            //요청을 통해 얻은 JSON타입의 Response 메세지 읽어오기
-            BufferedReader br = new BufferedReader(new InputStreamReader(conn.getInputStream()));
-            String line = "";
-            String result = "";
-
-            while ((line = br.readLine()) != null) {
-                result += line;
-            }
-            System.out.println("response body : " + result);
-
-            //Gson 라이브러리로 JSON파싱
-            JsonParser parser = new JsonParser();
-            JsonElement element = parser.parse(result);
-            System.out.println(element.getAsJsonObject().get("response").getAsJsonObject().get("email"));
-
-
-            br.close();
+            String profileImageValue = String.valueOf(element.getAsJsonObject().get("response").getAsJsonObject().get("profile_image"));
+            String profileImage = profileImageValue.replaceAll("\\\\","" );
+            System.out.println("id : " + element.getAsJsonObject().get("response").getAsJsonObject().get("id"));
+            System.out.println("email : " + element.getAsJsonObject().get("response").getAsJsonObject().get("email"));
+            System.out.println("profile_image : " + element.getAsJsonObject().get("response").getAsJsonObject().get("profile_image"));
+            System.out.println(profileImage);
 
         } catch (IOException e) {
             e.printStackTrace();
         }
+    }
+
+    public JsonElement jsonElement(String reqURL, String token, String code, String state) throws IOException {
+        URL url = new URL(reqURL);
+        HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+
+        //POST 요청을 위해 기본값이 false인 setDoOutput을 true로
+        conn.setRequestMethod("POST");
+        conn.setDoOutput(true);
+
+        //전송할 header 작성, access_token전송
+        if (token == null) {
+            BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(conn.getOutputStream()));
+            String sb = "grant_type=authorization_code" +  // TODO grant_type 입력
+                    "&client_id=fNINb0JLOoWHKPO8p2HO" + // TODO client-id 입력
+                    "&client_secret=5PzjffB3yr" + // TODO client_secret 입력
+                    "&redirect_uri=http://localhost:3000/oauth/callback/naver" + // TODO 인가코드 받은 redirect_uri 입력
+                    "&code=" + code +
+                    "&state=" + state;
+            bw.write(sb);
+            bw.flush();
+            bw.close();
+        } else
+            conn.setRequestProperty("Authorization", "Bearer " + token);
+
+        //결과 코드가 200이라면 성공
+        int responseCode = conn.getResponseCode();
+        System.out.println("responseCode : " + responseCode);
+
+        //요청을 통해 얻은 JSON타입의 Response 메세지 읽어오기
+        BufferedReader br = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+        String line;
+        StringBuilder result = new StringBuilder();
+
+        while ((line = br.readLine()) != null) {
+            result.append(line);
+        }
+        br.close();
+
+        System.out.println("response body : " + result);
+
+        //Gson 라이브러리에 포함된 클래스로 JSON 파싱 객체 생성
+        return JsonParser.parseString(result.toString());
+    }
 }
