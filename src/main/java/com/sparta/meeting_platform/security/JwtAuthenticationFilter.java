9d280c9c007ba54090c960ce65ed575e61701e24
jeonghyeonuk <junghunwook456@naver.com>
2022-07-08 15:41:41 +0900

    test

--- src/main/java/com/sparta/meeting_platform/security/JwtAuthenticationFilter.java
+++ src/main/java/com/sparta/meeting_platform/security/JwtAuthenticationFilter.java
@@ -20,7 +20,7 @@ public class JwtAuthenticationFilter extends GenericFilterBean {
     @Override
     public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
         // 헤더에서 jwt 토큰 받아옴
-        String token = jwtTokenProvider.validateToken((HttpServletRequest) request.get);
+        String token = ((HttpServletRequest) request).getHeader("Authorization");
         // 유효한 토큰인지 확인
         if (token != null && jwtTokenProvider.validateToken(token)) {
             // 토큰이 유효하면 토큰으로부터 유저 정보를 받아와서 저장
