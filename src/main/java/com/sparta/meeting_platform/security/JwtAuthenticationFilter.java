8a4c36161fb973fb8cfb3e1033f649fc5fd58525
junghoonkim96 <0527wj@gmail.com>
2022-07-10 19:55:58 +0900

    security 병합

--- src/main/java/com/sparta/meeting_platform/security/JwtAuthenticationFilter.java
+++ src/main/java/com/sparta/meeting_platform/security/JwtAuthenticationFilter.java
@@ -1,9 +1,10 @@
 package com.sparta.meeting_platform.security;
 
+import io.jsonwebtoken.JwtException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.security.oauth2.jwt.JwtException;
+
 import org.springframework.web.filter.GenericFilterBean;
 
 import javax.servlet.FilterChain;
