7feecf800b9e647c6aaa6fb34f8a8166fc72da9f
junghoonkim96 <0527wj@gmail.com>
2022-07-20 23:25:46 +0900

    qr 인증 google파트 수정

--- src/main/java/com/sparta/meeting_platform/service/QrcodeCheckService.java
+++ src/main/java/com/sparta/meeting_platform/service/QrcodeCheckService.java
@@ -48,6 +48,7 @@ public class QrcodeCheckService {
     private final GoogleConfig googleConfig;
     private final JwtTokenProvider jwtTokenProvider;
     private final UserRoleCheckService userRoleCheckService;
+    private final SocialGoogleService socialGoogleService;
 
     @Transactional
     public ResponseEntity<FinalResponseDto<?>> qrcodeUserCheck(Long postId, LoginRequestDto loginRequestDto) {
@@ -83,63 +84,13 @@ public class QrcodeCheckService {
 
     @Transactional
     public ResponseEntity<FinalResponseDto<?>> googleLogin(String authCode, HttpServletResponse httpServletResponse, Long postId) throws JsonProcessingException {
-        RestTemplate restTemplate = new RestTemplate();
-
-        // Gooogle Oauth Access Token 요청용 Dto
-        GoogleLoginRequestDto googleLoginRequestDto = GoogleLoginRequestDto.builder()
-                .clientId(googleConfig.getGoogleClientId())     // API Console Credentials page 에서 가져온 클라이언트 ID
-                .clientSecret(googleConfig.getGoogleSecret())       // Credentials page 에서 가져온 API Console 클라이언트 보안 비밀번호
-                .code(authCode)     // 초기 요청에서 반환된 승인 코드
-                .redirectUri(googleConfig.getGoogleRedirectUri())       // 지정된 client_id의 API ConsoleCredentials page 에서 프로젝트에 나열된 리디렉션 URI중 하나
-                .grantType("authorization_code")        // OAuth 2.0 사양에 정의된 대로 이 필드 값을 authorization_code로 설정
-                .build();
-
-        // Http Header 설정
-        HttpHeaders headers = new HttpHeaders();
-        headers.setContentType(MediaType.APPLICATION_JSON);
-        HttpEntity<GoogleLoginRequestDto> httpRequestEntity = new HttpEntity<>(googleLoginRequestDto, headers);
-        ResponseEntity<String> apiResponseJson = restTemplate.postForEntity
-                (googleConfig.getGoogleAuthUrl() + "/token", httpRequestEntity, String.class);
-
-        // ObjectMapper를 통해 String to Object로 변환
-        ObjectMapper objectMapper = new ObjectMapper();
-        objectMapper.setPropertyNamingStrategy(PropertyNamingStrategy.SNAKE_CASE);
-        objectMapper.setSerializationInclusion(JsonInclude.Include.NON_NULL); // NULL이 아닌 값만 응답받기(NULL인 경우는 생략)
-        GoogleLoginResponseDto googleLoginResponseDto
-                = objectMapper.readValue(apiResponseJson.getBody(), new TypeReference<GoogleLoginResponseDto>() {
-        });
-
-        // 사용자의 정보는 JWT Token으로 저장되어 있고, Id_Token에 값을 저장한다.
-        String jwtToken = googleLoginResponseDto.getIdToken();
-
-        //======================= 1단계(액세스 토큰 요청) 끝 =======================
-
-        // JWT Token을 전달해 JWT 저장된 사용자 정보 확인
-        String requestUrl = UriComponentsBuilder.fromHttpUrl
-                (googleConfig.getGoogleAuthUrl() + "/tokeninfo").queryParam("id_token", jwtToken).toUriString();
-
-        String resultJson = restTemplate.getForObject(requestUrl, String.class);
-
-        GoogleLoginDto userInfoDto = new GoogleLoginDto();
-        if (resultJson != null) {
-            userInfoDto = objectMapper.readValue(resultJson, new TypeReference<GoogleLoginDto>() {
-            });
-        }
-        userRoleCheckService.userResignCheck(userInfoDto.getEmail());
-        User user = userRepository.findByUsername(userInfoDto.getEmail()).orElseThrow(
+        ResponseEntity<FinalResponseDto<?>> finalResponseDtoResponseEntity
+                = socialGoogleService.googleLogin(authCode,httpServletResponse);
+        String username = finalResponseDtoResponseEntity.getBody().getUsername();
+        User user = userRepository.findByUsername(username).orElseThrow(
                 () -> new UserApiException("해당 유저를 찾을 수 없습니다."));
-
         Post post = postRepository.findById(postId).orElseThrow(
                 () -> new PostApiException("존재하지 않는 게시물 입니다."));
-
-        UserDetailsImpl userDetails = new UserDetailsImpl(user);
-        Authentication authentication =
-                new UsernamePasswordAuthenticationToken(userDetails, null, userDetails.getAuthorities());
-        SecurityContextHolder.getContext().setAuthentication(authentication);
-
-        String jwt_token = jwtTokenProvider.generateJwtToken(userDetails);
-        headers.set("Authorization", "BEARER" + " " + jwt_token);
-        httpServletResponse.addHeader("Authorization", "Bearer" + " " + jwt_token);
         qrcodeConfirm(post,user);
         return new ResponseEntity<>(new FinalResponseDto<>(true, "만남 성공!!"), HttpStatus.OK);
     }
