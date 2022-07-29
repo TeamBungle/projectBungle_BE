ae017d8c4689703611f87f23fceb5745472110e2
jeonghyeonuk <103723109+Jeonghyeonuk@users.noreply.github.com>
2022-07-30 06:54:57 +0900

    테스트 : 무중단배포 테스트 21

--- scripts/health_check.sh
+++ scripts/health_check.sh
@@ -1,6 +1,5 @@
 # health_check.sh
-#
-# !/bin/bash
+# Crawl current connected port of WAS
 CURRENT_PORT=$(cat /home/ubuntu/service_url.inc | grep -Po '[0-9]+' | tail -1)
 TARGET_PORT=0
 
@@ -12,17 +11,15 @@ elif [ ${CURRENT_PORT} -eq 8082 ]; then
 else
   echo "> No WAS is connected to nginx"
   exit 1
-fi
 
+fi
 
 echo "> Start health check of WAS at 'http://127.0.0.1:${TARGET_PORT}' ..."
 
 for RETRY_COUNT in 1 2 3 4 5 6 7 8 9 10
-
 do
   echo "> #${RETRY_COUNT} trying..."
-  RESPONSE_CODE=$(curl -s -o /dev/null -w "%{http_code}" http://127.0.0.1:${TARGET_PORT}/auth)
-  echo "> RESPONSE_CODE?? ${RESPONSE_CODE}."
+  RESPONSE_CODE=$(curl -s -o /dev/null -w "%{http_code}" http://127.0.0.1:${TARGET_PORT}/health)
 
   if [ ${RESPONSE_CODE} -eq 200 ]; then
     echo "> New WAS successfully running"
@@ -30,6 +27,8 @@ do
   elif [ ${RETRY_COUNT} -eq 10 ]; then
     echo "> Health check failed."
     exit 1
+
   fi
   sleep 10
+
 done
