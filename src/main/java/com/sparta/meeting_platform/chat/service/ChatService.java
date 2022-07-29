b1cb5bfcf95cd7f9a92771793dff2ac78e49fb82
jeonghyeonuk <103723109+Jeonghyeonuk@users.noreply.github.com>
2022-07-30 04:46:05 +0900

    테스트 : 무중단 배포 테스트 15

--- src/main/java/com/sparta/meeting_platform/chat/service/ChatService.java
+++ src/main/java/com/sparta/meeting_platform/chat/service/ChatService.java
@@ -6,13 +6,11 @@ import com.sparta.meeting_platform.chat.dto.ChatMessageDto;
 import com.sparta.meeting_platform.chat.dto.FilesDto;
 import com.sparta.meeting_platform.chat.dto.UserDetailDto;
 import com.sparta.meeting_platform.chat.dto.UserinfoDto;
-import com.sparta.meeting_platform.chat.model.ChatMessage;
-import com.sparta.meeting_platform.chat.model.ChatRoom;
-import com.sparta.meeting_platform.chat.model.InvitedUsers;
-import com.sparta.meeting_platform.chat.model.ResignChatRoom;
+import com.sparta.meeting_platform.chat.model.*;
 import com.sparta.meeting_platform.chat.repository.*;
 import com.sparta.meeting_platform.domain.User;
 import com.sparta.meeting_platform.exception.UserApiException;
+import com.sparta.meeting_platform.repository.LikeRepository;
 import com.sparta.meeting_platform.repository.PostRepository;
 import com.sparta.meeting_platform.repository.UserRepository;
 import com.sparta.meeting_platform.security.UserDetailsImpl;
@@ -44,6 +42,8 @@ public class ChatService {
     private final ChatRoomJpaRepository chatRoomJpaRepository;
     private final ResignChatRoomJpaRepository resignChatRoomJpaRepository;
     private final PostRepository postRepository;
+    private final LikeRepository likeRepository;
+    private final ResignChatMessageJpaRepository resignChatMessageJpaRepository;
 
 
     @Transactional
@@ -92,6 +92,19 @@ public class ChatService {
                     messageDto.setQuitOwner(true);
                     messageDto.setMessage("(방장) " + messageDto.getSender() + "님이 나가셨습니다. " +
                             "더 이상 대화를 할 수 없으며 채팅방을 나가면 다시 입장할 수 없습니다.");
+                    likeRepository.deleteByPostId(Long.parseLong(messageDto.getRoomId()));
+                    postRepository.deleteById(Long.parseLong(messageDto.getRoomId()));
+                    user.setIsOwner(false);
+                    ChatRoom findChatRoom = chatRoomJpaRepository.findByRoomId(messageDto.getRoomId());
+                    List<ChatMessage> chatMessage = chatMessageJpaRepository.findAllByRoomId(messageDto.getRoomId());
+                    ResignChatRoom resignChatRoom = new ResignChatRoom(findChatRoom);
+                    resignChatRoomJpaRepository.save(resignChatRoom);
+                    for (ChatMessage message : chatMessage) {
+                        ResignChatMessage resignChatMessage = new ResignChatMessage(message);
+                        resignChatMessageJpaRepository.save(resignChatMessage);
+                    }
+                    chatMessageJpaRepository.deleteByRoomId(messageDto.getRoomId());
+                    chatRoomJpaRepository.deleteByRoomId(messageDto.getRoomId());
                 }
             }
             chatMessageJpaRepository.deleteByRoomId(messageDto.getRoomId());
