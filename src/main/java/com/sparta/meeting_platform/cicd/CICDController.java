a4f5e0cd15cf9f211e712c13fc534a7c18ee579e
jeonghyeonuk <103723109+Jeonghyeonuk@users.noreply.github.com>
2022-07-30 14:52:31 +0900

    테스트 : 무중단 배포 테스트 31

--- src/main/java/com/sparta/meeting_platform/cicd/CICDController.java
+++ src/main/java/com/sparta/meeting_platform/cicd/CICDController.java
@@ -4,13 +4,11 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RequiredArgsConstructor
 @RestController
-public class CicdController {
-
+@RequiredArgsConstructor
+public class CICDController {
     @GetMapping("/health")
     public String checkHealth() {
         return "healthy";
     }
-
 }
