2fbec041bbe3af1bb773985707f73867b1b55784
junghoonkim96 <0527wj@gmail.com>
2022-07-30 15:40:50 +0900

    수정 : git repository 이동

--- src/main/java/com/sparta/meeting_platform/repository/LikeRepository.java
+++ src/main/java/com/sparta/meeting_platform/repository/LikeRepository.java
@@ -1,7 +1,6 @@
 package com.sparta.meeting_platform.repository;
 
 import com.sparta.meeting_platform.domain.Like;
-import com.sparta.meeting_platform.repository.mapping.Post;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
@@ -9,7 +8,6 @@ import java.util.Optional;
 
 public interface LikeRepository extends JpaRepository<Like, Long> {
     Optional<Like> findByUser_IdAndPost_Id(Long postId, Long userId);
-    List<Post> findAllByUserIdAndIsLikeTrueOrderByPost_Id(Long userId);
     void deleteByUserId(Long userId);
     void deleteByPostId(Long postId);
 
